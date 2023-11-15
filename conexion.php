7126237b41bb850e3ffbbd3ecb81b5abd8b070ba
joeramirezcorrea <146049521+joeramirezcorrea@users.noreply.github.com>
2023-11-14 19:59:35 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@ $usuario = sa;
 $password = 12345678;
 $base_datos = tienda-gorros;
 
-$conexion = new mysqli($LAPTOP-27FSTFC5\SQLEXPRESS, $sa, $12345678, $tienda-gorros);
+$conexion = new mysqli(LAPTOP-27FSTFC5\SQLEXPRESS, $sa, 12345678, $tienda-gorros);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);

06bc482e655b32abb85c7b54f4ecea45b99a8567
joeramirezcorrea <146049521+joeramirezcorrea@users.noreply.github.com>
2023-11-14 19:39:59 -0500

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -1,1 +1,17 @@
+<?php
+include('conexion.php');
 
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$query = "INSERT INTO usuarios (nombre, email, password) VALUES ('$nombre', '$email', '$password')";
+
+if ($conexion->query($query) === TRUE) {
+    echo "Registro exitoso";
+} else {
+    echo "Error al registrar: " . $conexion->error;
+}
+
+$conexion->close();
+?>
